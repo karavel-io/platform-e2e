e1a867254c03b9d397a09383edda44bf95c679c0
Matteo Joliveau <matteojoliveau@gmail.com>
2021-11-04 14:23:24 +0100

    Don't generate HTML report in Sonobuoy
    Signed-off-by: Matteo Joliveau <matteojoliveau@gmail.com>

--- tests/run.sh
+++ tests/run.sh
@@ -2,9 +2,5 @@
 
 results_dir="${RESULTS_DIR:-/tmp/results}"
 mkdir -p ${results_dir}
-go tool test2json -p github.com/karavel/e2e/tests/pkg -t e2e.test -test.v | tee report.log
-cat report.log | go-test-report --title 'Karavel E2E Tests' --output report.html
-mv report.log report.html ${results_dir}
-cd ${results_dir}
-tar czf results.tar.gz *
-echo ${results_dir}/results.tar.gz > ${results_dir}/done
+go tool test2json -p github.com/karavel/e2e/tests/pkg -t e2e.test -test.v | tee ${results_dir}/report.log
+echo ${results_dir}/report.log > ${results_dir}/done
