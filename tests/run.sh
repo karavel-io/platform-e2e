39ceb981b7a8d2b359062ec37d47fc8039f510e4
Matteo Joliveau <matteojoliveau@gmail.com>
2021-11-04 13:41:55 +0100

    Fix report title
    Signed-off-by: Matteo Joliveau <matteojoliveau@gmail.com>

--- tests/run.sh
+++ tests/run.sh
@@ -4,6 +4,6 @@ results_dir="${RESULTS_DIR:-/tmp/results}"
 mkdir -p ${results_dir}
 cd ${results_dir}
 go tool test2json -p github.com/karavel/e2e/tests/pkg -t e2e.test -test.v | tee report.log
-cat report.log | go-test-report --title Karavel E2E Tests --output report.html
+cat report.log | go-test-report --title 'Karavel E2E Tests' --output report.html
 tar czf results.tar.gz *
 echo ${results_dir}/results.tar.gz > ${results_dir}/done
