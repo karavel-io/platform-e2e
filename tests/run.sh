7df1a1bf3233515afdc9f88ebbb5a62d8d67e2e2
Matteo Joliveau <matteojoliveau@gmail.com>
2021-11-04 13:34:38 +0100

    Use go-json format
    Signed-off-by: Matteo Joliveau <matteojoliveau@gmail.com>

--- tests/run.sh
+++ tests/run.sh
@@ -3,7 +3,7 @@
 results_dir="${RESULTS_DIR:-/tmp/results}"
 mkdir -p ${results_dir}
 cd ${results_dir}
-e2e.test -test.v | tee report.log
-cat report.log | go-junit-report > report.xml
+go tool test2json -p github.com/karavel/e2e/tests/pkg -t e2e.test -test.v | tee report.log
+cat report.log | go-test-report --title Karavel E2E Tests --output report.html
 tar czf results.tar.gz *
 echo ${results_dir}/results.tar.gz > ${results_dir}/done
