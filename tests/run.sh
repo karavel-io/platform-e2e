7f9f06c85a0dfda13e15dddf692fbef128c21ba1
Matteo Joliveau <matteojoliveau@gmail.com>
2021-11-04 14:49:42 +0100

    Fix results dir
    Signed-off-by: Matteo Joliveau <matteojoliveau@gmail.com>

--- tests/run.sh
+++ tests/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-results_dir="${RESULTS_DIR:-/tmp/results}"
+results_dir="${SONOBUOY_RESULTS_DIR:-/tmp/sonobuoy/results}"
 mkdir -p ${results_dir}
 go tool test2json -p github.com/karavel/platform-e2e/tests/pkg -t e2e.test -test.v | tee ${results_dir}/report.log
 echo ${results_dir}/report.log > ${results_dir}/done
